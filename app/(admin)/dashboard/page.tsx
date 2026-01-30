c9c3c75277e4fee7525c0cddf952dc0630faea01
adugbo aghogho <adugboaghogho@gmail.com>
2026-01-30 11:01:35 +0100

    vercel error fix take 2

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,11 +1,3 @@
-import React from "react";
-
-const page = () => {
-  return (
-    <div>
-      <h2>Admin Dashboard</h2>
-    </div>
-  );
-};
-
-export default page;
+export default function OrdersPage() {
+  return <div>Orders Management</div>;
+}
